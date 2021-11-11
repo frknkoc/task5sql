4b01723cd377c04c6c0ccb67ed97b5ef33135ae9
Furkan Koç <93766996+frknkoc@users.noreply.github.com>
2021-11-11 12:59:13 +0300

    Create task5.sql

---
+++ task5.sql
@@ -0,0 +1,5 @@
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC;
+
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length OFFSET 5 LIMIT 5;
+
+SELECT last_name FROM customer WHERE store_id = 1 ORDER BY last_name LIMIT 4;
